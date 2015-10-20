f6329aaa1941ef8f6a113c63671d8e61665b7f65
Luan Nico <luanpotter27@gmail.com>
2015-10-20 08:50:46 -0200

    JavaFX

--- life/src/main/java/xyz/luan/life/Main.java
+++ life/src/main/java/xyz/luan/life/Main.java
@@ -1,7 +1,33 @@
 package xyz.luan.life;
 
-public class Main {
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+public class Main extends Application {
 	public static void main(String[] args) {
-		System.out.println("Hello World!");
+		launch(args);
+	}
+
+	@Override
+	public void start(Stage stage) {
+		stage.setTitle("Hello World!");
+		stage.setScene(new Scene(rootPane(), 300, 250));
+		stage.show();
+	}
+
+	private StackPane rootPane() {
+		StackPane root = new StackPane();
+		root.getChildren().add(simpleButton());
+		return root;
+	}
+
+	private Button simpleButton() {
+		Button btn = new Button();
+		btn.setText("Say 'Hello World'");
+		btn.setOnAction(e -> System.out.println("Hello World!"));
+		return btn;
 	}
 }
