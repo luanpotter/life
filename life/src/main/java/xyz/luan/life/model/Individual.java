98dcbad06aa3e5e13128da2b8386338581b07284
Luan Nico <luanpotter27@gmail.com>
2015-10-28 18:09:12 -0200

    new genome

--- life/src/main/java/xyz/luan/life/model/Individual.java
+++ life/src/main/java/xyz/luan/life/model/Individual.java
@@ -14,10 +14,10 @@ import xyz.luan.life.model.genetics.ReproductionGene;
 
 public class Individual extends Entity {
 
-	private Genome genome;
-	private int tickAge = 0;
-	private long timeAge = System.currentTimeMillis();
-	private int generation = 0;
+    private Genome genome;
+    private int tickAge = 0;
+    private long timeAge = System.currentTimeMillis();
+    private int generation = 0;
 
     private static class LazyIntersection {
 
@@ -37,115 +37,115 @@ public class Individual extends Entity {
         }
     }
 
-	private static EntityShape generateBody(Point2D position, Genome genome, int precision) {
-		List<Gene2> morfologicalGenes = Arrays.asList(Gene2.A, Gene2.B, Gene2.C, Gene2.D, Gene2.E, Gene2.F, Gene2.G, Gene2.H, Gene2.I, Gene2.J, Gene2.K, Gene2.L, Gene2.M,
-		        Gene2.N);
-		double[] characteristics = morfologicalGenes.stream().map(g -> genome.getGenes().get(g)).mapToDouble(Double::doubleValue).toArray();
+    private static EntityShape generateBody(Point2D position, Genome genome, int precision) {
+        List<Gene2> morfologicalGenes = Arrays.asList(Gene2.A, Gene2.B, Gene2.C, Gene2.D, Gene2.E, Gene2.F, Gene2.G, Gene2.H, Gene2.I, Gene2.J, Gene2.K,
+                Gene2.L, Gene2.M, Gene2.N);
+        double[] characteristics = morfologicalGenes.stream().map(g -> genome.getGenes().get(g)).mapToDouble(Double::doubleValue).toArray();
         EntityShape body = new EntityShape(position, characteristics, precision);
-        genome.getTranslationGene().initialSpeed(body);
-        genome.getColorGene().dye(body);
+        genome.getTranslation().initialSpeed(body);
+        genome.getColor().dye(body);
         return body;
     }
 
     public static Individual abiogenesis(Dimension2D dimension) {
         Random r = new Random();
         return new Individual(new Point2D(r.nextInt((int) dimension.getWidth()), r.nextInt((int) dimension.getHeight())), 50000, new Genome());
-	}
+    }
 
-	private Individual(Point2D position, double energy, Genome genome) {
-		super(Individual.generateBody(position, genome, 100), energy);
+    private Individual(Point2D position, double energy, Genome genome) {
+        super(Individual.generateBody(position, genome, 100), energy);
 
         this.body.toFront();
-		this.genome = genome;
-	}
+        this.genome = genome;
+    }
 
-	public Genome getGenome() {
-		return genome;
-	}
+    public Genome getGenome() {
+        return genome;
+    }
 
-	public double divide() {
-		double amount = genome.getReproductionGene().careCost(body);
-		this.loseEnergy(genome.getReproductionGene().reproductionCost(body));
-		return amount;
-	}
+    public double divide() {
+        double amount = genome.getReproduction().careCost(body);
+        this.loseEnergy(genome.getReproduction().reproductionCost(body));
+        return amount;
+    }
 
-	public boolean isAvailableToReproduce() {
-		return genome.getReproductionGene().isAvailableToReproduce(body, energy);
-	}
+    public boolean isAvailableToReproduce() {
+        return genome.getReproduction().isAvailableToReproduce(body, energy);
+    }
 
-	private Individual reproduce(Individual pair, Shape intersection) {
-		Random random = new Random();
-		Genome genome = new Genome();
-		for (Gene2 gene : this.getGenome().getGenes().keySet()) {
-			double a = this.getGenome().get(gene);
-			double b = pair.getGenome().get(gene);
-			double diff = Math.abs(a - b);
-			double mix = Math.min(a, b) + diff * random.nextDouble();
-			if (random.nextInt(Util.RARITY_OF_IMMUTABILITY) == 0) {
-				mix = mix + random.nextDouble() * Math.pow(-1, random.nextInt(1));
-			}
-			genome.getGenes().put(gene, Math.abs(mix));
-		}
+    private Individual reproduce(Individual pair, Shape intersection) {
+        Random random = new Random();
+        Genome genome = new Genome();
+        for (Gene2 gene : this.getGenome().getGenes().keySet()) {
+            double a = this.getGenome().get(gene);
+            double b = pair.getGenome().get(gene);
+            double diff = Math.abs(a - b);
+            double mix = Math.min(a, b) + diff * random.nextDouble();
+            if (random.nextInt(Util.RARITY_OF_IMMUTABILITY) == 0) {
+                mix = mix + random.nextDouble() * Math.pow(-1, random.nextInt(1));
+            }
+            genome.getGenes().put(gene, Math.abs(mix));
+        }
 
-		double initialEnergy = this.divide() + pair.divide();
-		Bounds bounds = intersection.getBoundsInParent();
-		Point2D center = new Point2D((bounds.getMaxX() + bounds.getMinX()) / 2, (bounds.getMaxY() + bounds.getMinY()) / 2);
-		Individual child = new Individual(center, initialEnergy, genome);
-		child.generation = Math.max(this.generation, pair.generation) + 1;
-		return child;
-	}
+        double initialEnergy = this.divide() + pair.divide();
+        Bounds bounds = intersection.getBoundsInParent();
+        Point2D center = new Point2D((bounds.getMaxX() + bounds.getMinX()) / 2, (bounds.getMaxY() + bounds.getMinY()) / 2);
+        Individual child = new Individual(center, initialEnergy, genome);
+        child.generation = Math.max(this.generation, pair.generation) + 1;
+        return child;
+    }
 
-	public void tryToReproduce(Entity entity, EntityManager em, LazyIntersection intersection) {
-		if (this.isAvailableToReproduce() && entity instanceof Individual) {
-			if (((Individual) entity).isAvailableToReproduce()
-			        && genome.geneticDistance(((Individual) entity).genome) < Util.ACCEPTABLE_GENETIC_DISTANCE_TO_REPRODUCE) {
-				if (intersection.getShape() != null && intersection.getShape().getLayoutBounds().getHeight() > 0
-				        && intersection.getShape().getLayoutBounds().getWidth() > 0) {
-					Individual child = reproduce((Individual) entity, intersection.getShape());
-					em.add(child);
-				}
-			}
-		}
-	}
+    public void tryToReproduce(Entity entity, EntityManager em, LazyIntersection intersection) {
+        if (this.isAvailableToReproduce() && entity instanceof Individual) {
+            if (((Individual) entity).isAvailableToReproduce()
+                    && genome.geneticDistance(((Individual) entity).genome) < Util.ACCEPTABLE_GENETIC_DISTANCE_TO_REPRODUCE) {
+                if (intersection.getShape() != null && intersection.getShape().getLayoutBounds().getHeight() > 0
+                        && intersection.getShape().getLayoutBounds().getWidth() > 0) {
+                    Individual child = reproduce((Individual) entity, intersection.getShape());
+                    em.add(child);
+                }
+            }
+        }
+    }
 
-	public void tryToEat(Entity entity, EntityManager em, LazyIntersection intersection) {
-		if (Util.ACCEPTABLE_AREA_PROPORTION_TO_EAT < this.getArea() / entity.getArea()) {
-			double cost = Util.BASE_METABOLIZATION_ENERGY_COST * entity.getArea();
-			if (this.getTotalEnergy() >= cost) {
-				if (intersection.getShape() != null && intersection.getShape().getLayoutBounds().getHeight() > 0
-				        && intersection.getShape().getLayoutBounds().getWidth() > 0) {
-					this.loseEnergy(cost);
-					this.gainEnergy(entity.getTotalEnergy());
-					em.remove(entity);
-				}
-			}
-		}
-	}
+    public void tryToEat(Entity entity, EntityManager em, LazyIntersection intersection) {
+        if (Util.ACCEPTABLE_AREA_PROPORTION_TO_EAT < this.getArea() / entity.getArea()) {
+            double cost = Util.BASE_METABOLIZATION_ENERGY_COST * entity.getArea();
+            if (this.getTotalEnergy() >= cost) {
+                if (intersection.getShape() != null && intersection.getShape().getLayoutBounds().getHeight() > 0
+                        && intersection.getShape().getLayoutBounds().getWidth() > 0) {
+                    this.loseEnergy(cost);
+                    this.gainEnergy(entity.getTotalEnergy());
+                    em.remove(entity);
+                }
+            }
+        }
+    }
 
-	private Food onDeath() {
-		System.out.println("death { tick: " + tickAge + " time: " + (System.currentTimeMillis() - timeAge) + " generation: " + generation + " }");
-		return new Food(this);
-	}
+    private Food onDeath() {
+        System.out.println("death { tick: " + tickAge + " time: " + (System.currentTimeMillis() - timeAge) + " generation: " + generation + " }");
+        return new Food(this);
+    }
 
-	@Override
-	public void onCollide(Entity entity, EntityManager em) {
-		LazyIntersection intersection = new LazyIntersection(this, entity);
+    @Override
+    public void onCollide(Entity entity, EntityManager em) {
+        LazyIntersection intersection = new LazyIntersection(this, entity);
         tryToReproduce(entity, em, intersection);
         tryToEat(entity, em, intersection);
-	}
+    }
 
-	@Override
-	public void tick(EntityManager em) {
-		tickAge++;
-		this.loseEnergy(Util.BASE_LIFE_ENERGY_COST * this.getArea());
+    @Override
+    public void tick(EntityManager em) {
+        tickAge++;
+        this.loseEnergy(Util.BASE_LIFE_ENERGY_COST * this.getArea());
 
-		if (this.getEnergy() < 0) {
-			em.remove(this);
-			em.add(onDeath());
-			return;
-		}
+        if (this.getEnergy() < 0) {
+            em.remove(this);
+            em.add(onDeath());
+            return;
+        }
 
-        genome.getTranslationGene().translate(body);
-	}
+        genome.getTranslation().translate(body);
+    }
 
 }
--- life/src/main/java/xyz/luan/life/model/genetics/Genome.java
+++ life/src/main/java/xyz/luan/life/model/genetics/Genome.java
@@ -1,74 +1,98 @@
 package xyz.luan.life.model.genetics;
 
-import xyz.luan.life.model.Gene2;
-import xyz.luan.life.model.Util;
-
 import java.util.HashMap;
 import java.util.Map;
 
+import xyz.luan.life.model.Gene2;
+
 public class Genome {
 
-    private Map<Gene2, Double> genes;
-    private TranslationGene translationGene;
-    private ColorGene colorGene;
-    private ReproductionGene reproductionGene;
+    private Map<Gene2, Double> genes2;
+
+    private Map<Class<? extends Gene<?>>, Gene<?>> genes;
 
     public Genome() {
         genes = new HashMap<>();
+        genes2 = new HashMap<>();
         randomGenes();
     }
 
-    private void randomGenes() {
-        genes.put(Gene2.ROTATION_SPEED, 0.5);
-        genes.put(Gene2.ROTATION_CONSTANCY, 0.5);
-
-        genes.put(Gene2.A, 10d); genes.put(Gene2.B, 1d);
-        genes.put(Gene2.C, 0d); genes.put(Gene2.D, 0d); genes.put(Gene2.E, 0d);
-        genes.put(Gene2.F, 10d); genes.put(Gene2.G, 1d);
-        genes.put(Gene2.H, 3d); genes.put(Gene2.I, 10d);
-        genes.put(Gene2.J, 0d); genes.put(Gene2.K, 0d); genes.put(Gene2.L, 0d);
-        genes.put(Gene2.M, 1d); genes.put(Gene2.N, 40d);
-
-        this.translationGene = new TranslationGene();
-        this.colorGene = new ColorGene();
-        this.reproductionGene = new ReproductionGene();
-    }
-
-    public double get(Gene2 gene) {
-        return gene.getCoefficient() * genes.get(gene);
-    }
-
-    public int numberOfGenes() {
-        return genes.size();
-    }
-
-    public Map<Gene2, Double> getGenes() {
-        return genes;
-    }
-
-    public double geneticDistance(Genome genome) {
-        //boolean sameGeneTypes = this.getGenes().keySet().equals(genome.getGenes());
-        boolean sameGeneTypes = true;
-        if (sameGeneTypes) {
-            double sum = 0;
-            for (Gene2 gene : this.getGenes().keySet()) {
-                sum += Math.abs(Math.pow(this.get(gene) - genome.get(gene), 2));
-            }
-            return sum;
-        } else {
-            return Util.INFINITY;
+    public Genome(Genome genome1, Genome genome2) {
+        genes = new HashMap<>();
+        for (Class<? extends Gene<?>> gene : genome1.genes.keySet()) {
+            // genes.put(gene, genome1.get(gene).meiosis(genome2.get(gene)));
         }
     }
 
-    public TranslationGene getTranslationGene() {
-        return this.translationGene;
+    private void randomGenes() {
+        genes2.put(Gene2.A, 10d);
+        genes2.put(Gene2.B, 1d);
+        genes2.put(Gene2.C, 0d);
+        genes2.put(Gene2.D, 0d);
+        genes2.put(Gene2.E, 0d);
+        genes2.put(Gene2.F, 10d);
+        genes2.put(Gene2.G, 1d);
+        genes2.put(Gene2.H, 3d);
+        genes2.put(Gene2.I, 10d);
+        genes2.put(Gene2.J, 0d);
+        genes2.put(Gene2.K, 0d);
+        genes2.put(Gene2.L, 0d);
+        genes2.put(Gene2.M, 1d);
+        genes2.put(Gene2.N, 40d);
+
+        genes.put(TranslationGene.class, new TranslationGene());
+        // genes.put(RotationGene.class, new RotationGene());
+        genes.put(ColorGene.class, new ColorGene());
+        genes.put(ReproductionGene.class, new ReproductionGene());
+        genes.put(MorfologicGene.class, new MorfologicGene());
     }
 
-    public ColorGene getColorGene() {
-        return this.colorGene;
+    public double get(Gene2 gene) {
+        return gene.getCoefficient() * genes2.get(gene);
     }
 
-    public ReproductionGene getReproductionGene() {
-        return this.reproductionGene;
+    public int numberOfGenes() {
+        return genes2.size();
+    }
+
+    public Map<Gene2, Double> getGenes() {
+        return genes2;
+    }
+
+    public double geneticDistance(Genome genome) {
+        double sum = 0;
+        for (Gene2 gene : this.getGenes().keySet()) {
+            sum += Math.abs(Math.pow(this.get(gene) - genome.get(gene), 2));
+        }
+        return sum;
+    }
+
+    @SuppressWarnings("unchecked")
+    public <T extends Gene<?>> T get(Class<T> clazz) {
+        return (T) genes.get(clazz);
+    }
+
+    public TranslationGene getTranslation() {
+        return get(TranslationGene.class);
+    }
+
+    public Gene getRotation() {
+        return null;// get(RotationGene.class);
+    }
+
+    public ColorGene getColor() {
+        return get(ColorGene.class);
+    }
+
+    public ReproductionGene getReproduction() {
+        return get(ReproductionGene.class);
+    }
+
+    public MorfologicGene getMorfological() {
+        return get(MorfologicGene.class);
+    }
+
+    public Genome meiosis(Genome genome) {
+        return new Genome(this, genome);
     }
 }
