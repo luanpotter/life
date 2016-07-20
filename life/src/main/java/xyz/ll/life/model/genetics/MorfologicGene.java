e90ff9c9a71eb7751ebc26af3fbf2591c02e3e35
Luan Nico <luanpotter27@gmail.com>
2016-07-20 20:28:15 -0300

    Removendo rectangulos

--- life/src/main/java/xyz/ll/life/model/genetics/MorfologicGene.java
+++ life/src/main/java/xyz/ll/life/model/genetics/MorfologicGene.java
@@ -64,11 +64,7 @@ public class MorfologicGene implements Gene<MorfologicGene> {
 
     public EntityShape generateShape(Point center) {
         List<Point> vertices = new ArrayList<>();
-        vertices.add(new Point(-10, -2));
-        vertices.add(new Point(10, -2));
-        vertices.add(new Point(10, 2));
-        vertices.add(new Point(-10, 2));
-        for (int i = 100000; i < MorfologicGene.MORFOLOGIC_PRECISION; i++) {
+        for (int i = 0; i < MorfologicGene.MORFOLOGIC_PRECISION; i++) {
             vertices.add(getPoint(i * MorfologicGene.ARC));
         }
         return new EntityShape(center, vertices);
